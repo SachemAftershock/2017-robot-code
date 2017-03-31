a5272e202640f4db1479602012eaebd6488d8c72
Dan Waxman <dan.waxman1@gmail.com>
2017-03-31 13:04:17 -0400

    Added and autonomous selector as backup
    This is for any failed autonomous selections. This way cameras aren't interfered with.

---
+++ VisionProcessing/Server/AutoSelector.py
@@ -0,0 +1,18 @@
+from networktables import NetworkTables
+
+NetworkTables.initialize(server='roboRIO-263-FRC.local')
+
+def main():
+	modeList = ['Middle With Shot - %d', 'Left Gear Forward - %d', 'Right Gear Forward - %d', 'Middle Gear No Shot - %d', 'Left Gear Still - %d', 'Right Gear Still - %d', 'Nothing - %d']
+	for i,e in enumerate(modeList):
+		print(e % i)
+	choice = int(input('What mode do you wanna do...\n'))
+	auto = NetworkTables.getTable('autoData')
+	auto.putString('mode', modeList[choice].replace(' - %d',''))
+	try:
+		input('Sending '  + modeList[choice].replace(' - %d','') + ' via networktables')
+	except Exception as e:
+		print('you fucked up: ' + str(e))
+
+if __name__ == '__main__':
+	main()
